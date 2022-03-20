d0339e927de1a686deb566e54413ea70e7441128
polizz <polizz@deaddrop.dev>
2022-03-19 22:32:10 -0500

    Use args

--- src/args.rs
+++ src/args.rs
@@ -10,14 +10,56 @@ pub struct Top {
 #[derive(FromArgs, Debug)]
 #[argh(subcommand)]
 pub enum SubCommand {
-    Watch(NotifyArgs)
+    Watch(NotifyArgs),
+    ListVars(ListArgs)
 }
 
 #[derive(FromArgs, Debug)]
-#[argh(subcommand, name = "notify", description = "Notify on NUT state changes")]
+#[argh(subcommand, name = "listvars", description = "List variables from ups")]
+pub struct ListArgs {
+  #[argh(option, description = "nut host", short = 'h', default = "String::from(\"localhost\")")]
+  pub nut_host: String,
+
+  #[argh(option, description = "nut host port", short = 't', default = "3493")]
+  pub nut_host_port: u16,
+
+  #[argh(option, description = "nut user", short = 'j')]
+  pub nut_user: String,
+
+  #[argh(option, description = "nut user password", short = 'x')]
+  pub nut_user_pass: String,
+}
+
+#[derive(FromArgs, Debug)]
+#[argh(subcommand, name = "watch", description = "Notify on NUT state changes")]
 pub struct NotifyArgs {
-    #[argh(option,  description = "push url", short = 'u')]
-    pub url: String,
-    #[argh(option, description = "poll duration", short = 'p')]
-    pub p: usize,
+    #[argh(option, description = "nut host", short = 'h', default = "String::from(\"localhost\")")]
+    pub nut_host: String,
+
+    #[argh(option, description = "nut host port", short = 't', default = "3493")]
+    pub nut_host_port: u16,
+
+    #[argh(option, description = "nut user", short = 'j')]
+    pub nut_user: String,
+
+    #[argh(option, description = "nut user password", short = 'x')]
+    pub nut_user_pass: String,
+
+    #[argh(option,  description = "notification URL", short = 'u')]
+    pub gotify_url: String,
+
+    #[argh(option, description = "ups name", short = 'b', default = "String::from(\"ups\")")]
+    pub ups_name: String,
+
+    #[argh(option, description = "seconds to poll ups status", short = 'i', default = "60")]
+    pub nut_polling_secs: u64,
+
+    #[argh(option, description = "ups variable that holds online status", short = 'v', default = "String::from(\"ups.status\")")]
+    pub ups_variable: String,
+
+    #[argh(option, description = "discharging status text", short = 'd', default = "String::from(\"OB DISCHRG\")")]
+    pub discharge_status_text: String,
+
+    #[argh(option, description = "charging status text", short = 'c', default = "String::from(\"OL CHRG\")")]
+    pub charge_status_text: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,75 @@
 mod args;
-// use args::*;
+use args::{Top, ListArgs, NotifyArgs }; //NotifyArgs
 
 use std::convert::TryInto;
 use std::{thread, time};
+use chrono::Local;
 use rups::blocking::Connection;
 use rups::{Auth, ConfigBuilder};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let nut_port = 3493;
-
-    let auth = Some(Auth::new(username, password));
-    let interval = time::Duration::from_secs(10);
-
+fn run_list_command(ListArgs { nut_host, nut_host_port, nut_user, nut_user_pass }: ListArgs)
+        -> Result<(), Box<dyn std::error::Error>> {
+    let auth = Some(Auth::new(nut_user, Some(nut_user_pass)));
     let config = ConfigBuilder::new()
-        .with_host((nut_host, nut_port).try_into().unwrap_or_default())
+        .with_host((nut_host, nut_host_port).try_into().unwrap_or_default())
         .with_auth(auth)
         .with_debug(false)
         .build();
+    let mut conn = Connection::new(&config)?;
 
-    let mut conn = Connection::new(&config).unwrap();
+    conn.list_ups()?
+    .iter()
+    .for_each(|(name, desc)| {
+        println!("UPS Name: {}, Description: {}", &name, &desc);
+
+        conn.list_vars(&name).unwrap()
+            .iter()
+            .for_each(|val| println!("\t- {}", &val))
+    });
+
+    Ok(())
+}
+
+fn watch(NotifyArgs { nut_host, nut_host_port, nut_user, nut_user_pass, gotify_url, ups_name, nut_polling_secs, ups_variable, discharge_status_text, charge_status_text }: NotifyArgs)
+        -> Result<(), Box<dyn std::error::Error>> {
+    let interval = time::Duration::from_secs(nut_polling_secs);
+    let auth = Some(Auth::new(nut_user, Some(nut_user_pass)));
+    let config = ConfigBuilder::new()
+        .with_host((nut_host, nut_host_port).try_into().unwrap_or_default())
+        .with_auth(auth)
+        .with_debug(false)
+        .build();
+    let mut conn = Connection::new(&config)?;
 
     let mut previous_status = String::new();
-    let on_battery: String = String::from("OB DISCHRG");
-    let on_line: String = String::from("OL CHRG");
 
     loop {
-        let ups_variable = conn.get_var("ups", "ups.status")?;
+        let ups_variable = conn.get_var(&ups_name[..], &ups_variable[..])?;
         let status = ups_variable.value();
 
-        println!("Got status => {}", &status);
+        println!("{} \t=> Got status => {}", Local::now().format("%Y-%m-%d][%H:%M:%S"), &status);
 
         if previous_status == "" {
             previous_status = status.clone();
             println!("INIT ON {}", &previous_status);
 
-            if previous_status == on_battery {
+            if previous_status == discharge_status_text {
                 let notice_params = [("message", "INIT - UPS ONBATT - Discharging"), ("priority", "1")];
-                notify(&notice_params)?;
+                notify(&gotify_url, &notice_params)?;
             }
         } else {
-            if status == on_line && previous_status == on_battery {
-                previous_status = on_line.clone();
+            if status == charge_status_text && previous_status == discharge_status_text {
+                previous_status = charge_status_text.clone();
     
                 println!("ONLINE");
                 let notice_params = [("message", "UPS ONLINE - Charging"), ("priority", "1")];
-                notify(&notice_params)?;
-            } else if status == on_battery && previous_status == on_line {
-                previous_status = on_battery.clone();
+                notify(&gotify_url, &notice_params)?;
+            } else if status == discharge_status_text && previous_status == charge_status_text {
+                previous_status = discharge_status_text.clone();
     
-                println!("ONBATT");
+                println!("ONBATT!!");
                 let notice_params = [("message", "UPS ONBATT - Discharging"), ("priority", "1")];
-                notify(&notice_params)?;
+                notify(&gotify_url, &notice_params)?;
             }
         }
 
@@ -58,29 +77,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 }
 
-fn notify(notice_params: &[(&str, &str); 2]) -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let top: Top = argh::from_env();
 
+    match top.command {
+        args::SubCommand::ListVars(args) => run_list_command(args),
+        args::SubCommand::Watch(args) => watch(args),
+    }
+}
+
+fn notify(gotify_url: &str, notice_params: &[(&str, &str); 2]) -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::blocking::Client::new();
-    let resp = client.post(&notification_url)
+    let _ = client.post(gotify_url)
         .form(&notice_params)
         .send()?;
 
-    println!("Got response: {:?}", resp.status());
-
     Ok(())
 }
-
-// enumerate all ups and their vars
-
-// conn.list_ups().unwrap()
-// .iter()
-// .for_each(|(name, desc)| {
-//     println!("{}, {}", &name, &desc);
-
-//     conn.list_vars(&name).unwrap()
-//         .iter()
-//         .filter(|variable| variable.to_string().starts_with("ups.status:"))
-//         .for_each(|val| println!("\t- {}", &val))
-//         // "ups.status: OL CHRG"
-//         //  OB DISCHRG
-// });
